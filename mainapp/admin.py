f4fd15365131c8e79ef854da883da59e12cf64ec
LeVaSiK-14 <xxxxxxbiglev@gmail.com>
2022-09-20 11:42:18 +0600

    initial commit

---
+++ mainapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from mainapp.models import *
+
+admin.site.register(School)
+admin.site.register(Director)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Lesson)
+admin.site.register(Clas)
+admin.site.register(Zauch)
