963b26741c0f67b37359b46b7e9fe00a4cee8224
LeVaSiK-14 <xxxxxxbiglev@gmail.com>
2022-09-20 12:59:54 +0600

    refactor code

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from mainapp.models import *
+# from mainapp.models import *
 
-admin.site.register(School)
-admin.site.register(Director)
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Lesson)
-admin.site.register(Clas)
-admin.site.register(Zauch)
+# admin.site.register(School)
+# admin.site.register(Director)
+# admin.site.register(Student)
+# admin.site.register(Teacher)
+# admin.site.register(Lesson)
+# admin.site.register(Clas)
+# admin.site.register(Zauch)
